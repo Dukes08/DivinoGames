82e36803e0002b9c0257c9791353741f56867cfd
Hugo Duque <hugoduque@Hugos-MacBook-Pro.local>
2024-03-09 19:56:47 -0400

    fix: firebase

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,16 +1,16 @@
 import {useRoutes, BrowserRouter} from 'react-router-dom'
-import Login from '../Login'
-import ClubDetails from '../ClubDetails'
-import Landing from '../Landing'
+// import Login from '../Login'
+// import ClubDetails from '../ClubDetails'
+// import Landing from '../Landing'
 import SignUp from '../SignUp'
 import './App.css'
 
 const AppRoutes = () =>{
   let routes = useRoutes([
-    {path: '/', element: <Login />}, 
+    // {path: '/', element: <Login />}, 
     {path: '/sign-up', element: <SignUp />},
-    {path: '/landing', element: <Landing />},
-    {path: '/club-details', element: <ClubDetails />}
+    // {path: '/landing', element: <Landing />},
+    // {path: '/club-details', element: <ClubDetails />}
   ])
 
   return routes;
---
+++ src/firebase/config.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
+import { getStorage } from "firebase/storage"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYbZvC9olPvQ0kvLgrRkqRomdZ0aheymU",
+  authDomain: "divinogames-47439.firebaseapp.com",
+  projectId: "divinogames-47439",
+  storageBucket: "divinogames-47439.appspot.com",
+  messagingSenderId: "686164158446",
+  appId: "1:686164158446:web:127f0fc5f3ef52af21d0ff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export const auth = getAuth(app);
+
+export const storage  = getStorage(app);
+
+export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({prompt: "select_account"});
