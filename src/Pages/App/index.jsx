bf73bcc749f0cfbad2f853c4808d805362c1e838
Hugo Duque <hugoduque@Hugos-MacBook-Pro.local>
2024-03-09 00:00:19 -0400

    feat: Login Page

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,16 +1,11 @@
 import {useRoutes, BrowserRouter} from 'react-router-dom'
 import Login from '../Login'
-import ClubDetails from '../ClubDetails'
-import Landing from '../Landing'
-import SignUp from '../SignUp'
 import './App.css'
 
 const AppRoutes = () =>{
   let routes = useRoutes([
     {path: '/', element: <Login />}, 
-    {path: '/sign-up', element: <SignUp />},
-    {path: '/landing', element: <Landing />},
-    {path: '/club-details', element: <ClubDetails />}
+    
   ])
 
   return routes;
