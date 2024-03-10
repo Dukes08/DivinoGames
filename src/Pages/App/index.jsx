f327063f99d22fe8bf45dc9c2dafcee22445fe0e
Hugo Duque <hugoduque@Hugos-MacBook-Pro.local>
2024-03-10 09:25:05 -0400

    fixed: prepared for merge

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,19 +1,19 @@
 import {useRoutes, BrowserRouter} from 'react-router-dom'
-//import Login from '../Login'
+import Login from '../Login'
 import ClubDetails from '../ClubDetails'
 import  Landing  from '../Landing'
 import SearchPage from '../SearchPage'
-//import SignUp from '../SignUp'
-//import Profile from '../Profile'
+import SignUp from '../SignUp'
+import Profile from '../Profile'
 import './App.css'
 
 const AppRoutes = () =>{
   let routes = useRoutes([
-  //  {path: '/', element: <Login />}, 
-    //{path: '/sign-up', element: <SignUp />},
+    {path: '/', element: <Login />}, 
+    {path: '/sign-up', element: <SignUp />},
     {path: '/landing', element: <Landing />},
     {path: '/club/:id', element: <ClubDetails />},
-    //{path: '/profile', element: <Profile />}
+    {path: '/profile', element: <Profile />},
     {path: '/buscador', element: <SearchPage />}
 
   ])
