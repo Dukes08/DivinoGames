9af9d387019d06f810bc4fd8eb84defa2a673130
andresrivasrod <andres.rivas@correo.unimet.edu.ve>
2024-03-11 13:22:27 -0400

    feat:Responsive and favorite game menu

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,121 +1,99 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import {
-  loginWithEmailAndPassword,
-  signInWithGoogle,
-} from "../../firebase/auth";
+import { loginWithEmailAndPassword, signInWithGoogle } from "../../firebase/auth";
+import Google_logo from '../../assets/Google_logo.svg.png';
 
-import Google_logo from '../../assets/Google_logo.svg.png'
 function Login() {
     const navigate = useNavigate();
     const [formData, setFormData] = useState({
-      email: "",
-      password: "",
+        email: "",
+        password: "",
     });
 
     const onSuccess = () => {
-      navigate("/landing");
+        navigate("/landing");
     };
 
     const onFail = (_error) => {
-      console.log("LOGIN FAILED, Try Again");
+        console.log("LOGIN FAILED, Try Again");
     };
 
     const onSubmit = async (event) => {
-      event.preventDefault();
-      console.log({formData})
-  
-      await loginWithEmailAndPassword({ userData: formData, onSuccess, onFail });
+        event.preventDefault();
+        await loginWithEmailAndPassword({ userData: formData, onSuccess, onFail });
     };
-  
+
     const onChange = (event) => {
-      setFormData((oldData) => ({
-        ...oldData,
-        [event.target.name]: event.target.value,
-      }));
+        setFormData((oldData) => ({
+            ...oldData,
+            [event.target.name]: event.target.value,
+        }));
     };
-  
+
     const handleGoogleClick = async () => {
-      await signInWithGoogle({
-        onSuccess: () => navigate("/landing"),
-      });
+        await signInWithGoogle({
+            onSuccess: () => navigate("/landing"),
+        });
     };
 
-
-  
     return (
-    
         <div className="w-full h-screen flex items-start">
 
-        <form onSubmit={onSubmit} className="w-full h-screen flex">
+            <form onSubmit={onSubmit} className="w-full h-screen flex flex-col md:flex-row">
 
-          <div className="relative w-1/2 h-full flex flex-col bg-emerald-400" >
+                <div className="relative w-full md:w-1/2 h-full flex flex-col bg-emerald-400">
 
-              <div className="absolute top-[30%] left-[10%] flex flex-col">
+                    <div className="absolute top-1/3 left-1/10 flex flex-col">
 
-                <h1 className="text-4xl text-black font-bold my-4">Watch The Games You Always Wanted</h1>
+                        <h1 className="text-4xl text-black font-bold my-4">Watch The Games You Always Wanted</h1>
 
-                <p className="text-x1 text-black font-normal mx-2">Be A Kid Again And Watch Those Games Of The Golden Age</p>
-              </div>
-          </div>
-
-
-           <div className="w-1/2 h-full bg-white flex flex-col p-20 justify-evenly">
-          
-                
-                  <h3 className="text-3xl font-semibold mb-4">Login</h3>
-                  <p className="text-sm mb-2">Welcome! Please enter your details.</p>
-
-                  
-                  <input 
-                  type="email" 
-                  name="email"
-                  placeholder="Email"
-                  onChange={onChange}
-                  className="w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus: outline-none"
-                  />
-
-                <input 
-                  type="password" 
-                  name="password"
-                  placeholder="Password"
-                  onChange={onChange}
-
-                  className="w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus: outline-none"
-                  />
-                
-                  <button type="submit" className="w-full text-black font-bold my-2 bg-emerald-400 rounded-md p-4 text-center flex justify-center cursor-pointer">
-                    Log In
-                  </button>
-
-                  <Link className="w-full" to="/sign-up">
-                  <button className="w-full text-emerald-400 font-bold my-2 bg-white border-2 border-emerald-400 rounded-md p-4 text-center flex justify-center cursor-pointer">Sign Up</button>
-                  </Link>
-                
-
-                <div className="w-full flex items-center justify-center relative py-2">
-                  <div className="w-full h-[1px] bg-black"></div>
-                  <p className="text-lg absolute text-black bg-white">or</p>
+                        <p className="text-xl text-black font-normal mx-2">Be A Kid Again And Watch Those Games Of The Golden Age</p>
+                    </div>
                 </div>
 
+                <div className="w-full md:w-1/2 h-full bg-white flex flex-col p-8 md:p-20 justify-evenly">
 
-                <button type="button" onClick={handleGoogleClick} className="w-full text=black my-2 font-semibold bg-white border-2 border-black rounded-md p-4 text-center flex justify-center cursor-pointer">
-                  <img src={Google_logo} alt="googlelogo" className="h-4 mt-1 mr-1" />
-                  Sign In With Google
-                </button>
+                    <h3 className="text-3xl font-semibold mb-4">Login</h3>
+                    <p className="text-sm mb-2">Welcome! Please enter your details.</p>
 
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        onChange={onChange}
+                        className="w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none"
+                    />
 
-                
-                {/* <Link to="/sign-up" className="w-full flex justify-center"><p className=" text-sm font-normal  "> Dont have a account <span className=" font-semibold underline underline-offset-2 cursor-pointer " >Sign Up</span></p>
-                </Link> */}
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        onChange={onChange}
+                        className="w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none"
+                    />
 
-            
-            </div>
-          </form>
+                    <button type="submit" className="w-full text-black font-bold my-2 bg-emerald-400 rounded-md p-4 text-center flex justify-center cursor-pointer">
+                        Log In
+                    </button>
+
+                    <Link to="/sign-up">
+                        <button className="w-full text-emerald-400 font-bold my-2 bg-white border-2 border-emerald-400 rounded-md p-4 text-center flex justify-center cursor-pointer">Sign Up</button>
+                    </Link>
+
+                    <div className="w-full flex items-center justify-center relative py-2">
+                        <div className="w-full h-[1px] bg-black"></div>
+                        <p className="text-lg absolute text-black bg-white">or</p>
+                    </div>
+
+                    <button type="button" onClick={handleGoogleClick} className="w-full text-black my-2 font-semibold bg-white border-2 border-black rounded-md p-4 text-center flex justify-center cursor-pointer">
+                        <img src={Google_logo} alt="googlelogo" className="h-4 mt-1 mr-1" />
+                        Sign In With Google
+                    </button>
+
+                </div>
+            </form>
         </div>
-        
-    
     )
-  }
-  
-  export default Login
+}
+
+export default Login;
--- src/Pages/SignUp/index.jsx
+++ src/Pages/SignUp/index.jsx
@@ -1,8 +1,9 @@
-import Google_logo from "../../assets/Google_logo.svg.png"
-import {Link, useNavigate} from "react-router-dom"
+import React from 'react'
+import { Link, useNavigate } from "react-router-dom"
 import { registerWithEmailAndPassword, signInWithGoogle } from "../../firebase/auth";
-import { useState } from "react";
-import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { getFirestore, collection, query, getDocs } from "firebase/firestore";
+import Google_logo from "../../assets/Google_logo.svg.png"
 
 function SignUp(){
     const navigate = useNavigate();
@@ -14,128 +15,93 @@ function SignUp(){
         email: "",
         password: "",
         favoritegame: "",
-    })
+    });
 
-  
+    const [availableGames, setAvailableGames] = useState([]);
 
-    const onSuccess = () => {
-        navigate("/landing");
-      };
-    
-    const onFail = (_error) => {
-        console.log("REGISTER FAILED, Try Again");
-      };
+    useEffect(() => {
+        const fetchGames = async () => {
+            const db = getFirestore();
+            const gamesRef = collection(db, "videojuegos");
+            const querySnapshot = await getDocs(gamesRef);
 
+            const gamesList = [];
+            querySnapshot.forEach((doc) => {
+                gamesList.push(doc.data().titulo);
+            });
+
+            setAvailableGames(gamesList);
+        };
+
+        fetchGames();
+    }, []);
 
     const handleOnchange = (event) =>{
         setFormData((oldData) => ({
             ...oldData,
             [event.target.name]: event.target.value,
-          }));
-        };
+        }));
+    };
+
+    const onSuccess = () => {
+        navigate("/landing");
+    };
+
+    const onFail = (_error) => {
+        console.log("REGISTER FAILED, Try Again");
+    };
 
     const handleGoogleClick = async () => {
         await signInWithGoogle({
-          onSuccess: () => navigate("/landing"),
+            onSuccess: () => navigate("/landing"),
         });
-      };
+    };
 
-      const onSubmit = async (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
 
-        // Verificar si el juego favorito está en la base de datos antes de registrar al usuario
-        const gameExistsInDatabase = await checkGameInDatabase(formData.favoritegame);
-
-        if (gameExistsInDatabase) {
-    
+        if (availableGames.includes(formData.favoritegame)) {
             await registerWithEmailAndPassword({ userData: formData, onSuccess, onFail });
         } else {
             alert("El juego favorito no se encuentra en la base de datos. No se puede registrar.");
-            // Puedes mostrar un mensaje al usuario indicando que el juego no está en la base de datos
         }
     };
 
-    // Verificar si el juego favorito está en la base de datos antes de registrar al usuario
-
-
-    const checkGameInDatabase = async (favoritegame) => {
-      const db = getFirestore();
-      const gamesRef = collection(db, "videojuegos");
-      const q = query(gamesRef, where("titulo", "==", favoritegame));
-      const querySnapshot = await getDocs(q);
-      
-      return !querySnapshot.empty;
-  };
-
-    // const onSubmit = async (event) => {
-    //    event.preventDefault();   
-    //    console.log({formData})
-    //    await registerWithEmailAndPassword({userData:formData, onSuccess, onFail})
-    // }
-
-
-
     return(
-        <div className="w-full h-screen bg-emerald-400 flex justify-center items-center">
-            <form onSubmit ={onSubmit} className="bg-white w-2/5 h-3/4 shadow-md shadow-slate-950 flex flex-col items-center">
-                <h1 className="text-3xl font-bold font-sans mt-12 text-emerald-400">Sign Up</h1>
+      <div className="w-full min-h-screen bg-emerald-400 flex justify-center items-center">
+      <form onSubmit={onSubmit} className="bg-white w-full md:w-2/3 lg:w-1/2 xl:w-1/3 p-8 rounded-lg shadow-md">
+          <h1 className="text-3xl font-bold font-sans mt-12 text-emerald-400 text-center">Sign Up</h1>
 
-                <div className="w-full flex flex-col items-center">
-                    <input type="text" placeholder="Name" name= "name"                     className=" w-4/5 
-                    text-black p-2 my-4 bg-transparent border-2 border-emerald-400 focus: outline-none"
-                    onChange={handleOnchange}/>
+          <div className="w-full flex flex-col items-center">
+              <input type="text" placeholder="Name" name="name" className="w-full md:w-4/5 text-black p-2 my-4 bg-transparent border-2 border-emerald-400 focus:outline-none" onChange={handleOnchange}/>
 
+              <input type="text" placeholder="Last Name" name="lastname" className="w-full md:w-4/5 text-black p-2 my-4 bg-transparent border-2 border-emerald-400 focus:outline-none" onChange={handleOnchange}/>
 
-                    <input type="text" placeholder="Last Name" name= "lastname"
-                    className=" w-4/5 text-black p-2 my-4 bg-transparent border-2 border-emerald-400 focus: outline-none"
-                    onChange={handleOnchange}/>
+              <input type="text" placeholder="Username" name="username" className="w-full md:w-4/5 text-black p-2 my-4 bg-transparent border-2 border-emerald-400 focus:outline-none" onChange={handleOnchange}/>
 
+              <input type="email" placeholder="Email" name="email" className="w-full md:w-4/5 text-black p-2 my-4 bg-transparent border-2 border-emerald-400 focus:outline-none" onChange={handleOnchange}/>
 
-                    <input type="text" placeholder="Username" name="username"
-                    className=" w-4/5 text-black p-2 my-4 bg-transparent border-2 border-emerald-400 focus: outline-none"
-                    onChange={handleOnchange}/>
+              <input type="password" name="password" placeholder="Password" className="w-full md:w-4/5 text-black p-2 my-4 bg-transparent border-2 border-emerald-400 focus:outline-none" onChange={handleOnchange}/>
 
+              <select name="favoritegame" onChange={handleOnchange} value={formData.favoritegame} className="w-full md:w-4/5 text-black p-2 my-4 bg-transparent border-2 border-emerald-400 focus:outline-none">
+                    <option value="" disabled>Select your favorite game</option>
+                    {availableGames.map((game) => (
+                        <option key={game} value={game}>{game}</option>
+                    ))}
+              </select>
+                  <button type="submit" className="bg-emerald-400 p-4 w-full md:w-4/5">Register</button>
+                  <button type="button" onClick={handleGoogleClick} className="w-full md:w-4/5 text-black my-2 font-semibold bg-white border-2 border-black rounded-md p-4 text-center flex justify-center cursor-pointer">
+                      <img src={Google_logo} alt="googlelogo" className="h-4 mt-1 mr-2" />
+                      Sign Up With Google
+                  </button>
 
-                    <input 
-                    type="email" placeholder="Email" name="email" 
-                    className=" w-4/5 
-                    text-black p-2 my-4 bg-transparent border-2 border-emerald-400 focus: outline-none" 
-                    onChange={handleOnchange}/>
-
-
-                    <input type="password"
-                    name="password" 
-                    placeholder="Password" 
-                    className=" w-4/5 text-black p-2 my-4 bg-transparent border-2 border-emerald-400 focus: outline-none"
-                    onChange={handleOnchange}/>
-
-
-                    <input type="text" 
-                    name="favoritegame"
-                    placeholder="Favorite Game" 
-                    className=" w-4/5 text-black p-2 my-4 bg-transparent border-2 border-emerald-400 focus: outline-none" onChange={handleOnchange}/>
-                </div>
-
-                <div className=" w-full text-center">
-
-                    <button type="submit" className="bg-emerald-400 p-4 w-4/5">Register</button>
-
-                </div>
-
-
-                <button type="button" onClick={handleGoogleClick} className="w-4/5 text=black my-2 font-semibold bg-white border-2 border-black rounded-md p-4 text-center flex justify-center cursor-pointer">
-                  <img src={Google_logo} alt="googlelogo" className="h-4 mt-1 mr-2" />
-                  Sign Up With Google
-                </button>
-
-                <Link to="/" className="mt-4">
-                Already have an account? <span className="text-emerald-400">Login</span>
-                </Link>
-
-                </form> 
-        </div>
-    )
-
+              <Link to="/" className="mt-4 block text-center">
+                  Already have an account? <span className="text-emerald-400">Login</span>
+              </Link>
+          </div>
+      </form> 
+  </div>
+    );
 }
 
-export default SignUp
+export default SignUp;
